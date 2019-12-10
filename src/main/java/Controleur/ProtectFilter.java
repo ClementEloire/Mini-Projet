aac9393a2cc48dd67aca0c25625431063f55672f
AlexisJalabert <alexis.jalabert@etud.univ-jfc.fr>
2019-12-10 14:38:52 +0100

    Création page filtre

---
+++ src/main/java/Controleur/ProtectFilter.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controleur;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author pedago
+ */
+@WebServlet(name = "ProtectFilter", urlPatterns = {"/ProtectFilter"})
+public class ProtectFilter implements Filter  {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
