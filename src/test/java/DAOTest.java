a4619daf1b7b54339c1e3ec6e664076dccdf2afb
AlexisJalabert <alexis.jalabert@etud.univ-jfc.fr>
2019-12-11 11:50:03 +0100

    Test méthode infoClient

--- src/test/java/DAOTest.java
+++ src/test/java/DAOTest.java
@@ -18,12 +18,14 @@ import Modele.*;
  */
 public class DAOTest {
     
+    DAO dao = new DAO(DataSourceFactory.getDataSource());
+    
     public DAOTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
-        DAO dao = new DAO(DataSourceFactory.getDataSource());
+        
     }
     
     @AfterClass
@@ -32,10 +34,28 @@ public class DAOTest {
     
     @Before
     public void setUp() {
+        
     }
     
     @After
     public void tearDown() {
+    }
+    
+    @Test
+    public void testInfoClient() throws Exception {
+        Client client = dao.infoClient("ALFKI");
+        System.out.println(client.getSociete());
+        Client client2 = new Client("ALFKI", "Alfreds Futterkiste", "Maria Anders", "Représentant(e)", "Obere Str. 57", "Berlin", null, "12209     ", "Allemagne", "030-0074321", "030-0076545");
+        assertEquals(client.getCode(),client2.getCode());
+        assertEquals(client.getSociete(),client2.getSociete());
+        assertEquals(client.getFonction(),client2.getFonction());
+        assertEquals(client.getAdresse(),client2.getAdresse());
+        assertEquals(client.getVille(),client2.getVille());
+        assertEquals(client.getRegion(),client2.getRegion());
+        assertEquals(client.getCodePostal(),client2.getCodePostal());
+        assertEquals(client.getPays(),client2.getPays());
+        assertEquals(client.getTel(),client2.getTel());
+        assertEquals(client.getFax(),client2.getFax());
     }
 
     // TODO add test methods here.
